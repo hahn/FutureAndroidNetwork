54389188856450adb7f2576bbb952a0e89b65935
Hanhan Husna Firmansyah <hanhan.firmanysah@gdn-commerce.com>
2021-01-20 20:19:46 +0700

    branch okhttp

--- app/src/main/java/com/example/futureandroidnetwork/MainActivity.kt
+++ app/src/main/java/com/example/futureandroidnetwork/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.futureandroidnetwork
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.futureandroidnetwork.feature2.Feature2Activity
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    findViewById<Button>(R.id.bt_feature2).setOnClickListener {
+      startActivity(Intent(this, Feature2Activity::class.java))
+    }
   }
 }
---
+++ app/src/main/java/com/example/futureandroidnetwork/feature2/Feature2Activity.kt
@@ -0,0 +1,65 @@
+package com.example.futureandroidnetwork.feature2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.futureandroidnetwork.R
+import okhttp3.*
+import java.io.IOException
+
+class Feature2Activity : AppCompatActivity() {
+
+    private var downloading = false
+    private val url = "https://reqres.in/api/users/2"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature2)
+
+        findViewById<Button>(R.id.bt_download).setOnClickListener {
+            if (!downloading) {
+                downloading = true
+
+                runOnUiThread {
+                    startDownloadUrl()
+                }
+            }
+        }
+    }
+
+    private fun startDownloadUrl() {
+        val client = OkHttpClient()
+        var result = ""
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                downloading = false
+                result = "error connection: ${e.message}"
+                printResult(result)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                downloading = false
+                response.run {
+                    result = if (!isSuccessful) {
+                        "error code: $code"
+                    } else {
+                        body?.string() ?: "empty"
+                    }
+                    printResult(result)
+                }
+            }
+
+
+        })
+    }
+
+    private fun printResult(resultString: String) {
+        runOnUiThread {
+            findViewById<TextView>(R.id.tv_result).text = "result: $resultString"
+        }
+    }
+}
